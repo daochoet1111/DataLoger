e03918b79fa4db06a52924242d7703da2c302e30
Nguyễn Văn Đạo - K63 <88228722+daochoet1111@users.noreply.github.com>
2023-02-22 11:34:14 +0700

    .

--- src/main.py
+++
@@ -1,6 +0,0 @@
-from hmi.atBiography import App
-
-if __name__ == "__main__":
-    print("hello world")
-    app = App()
-    app.start()  
--- test/Icon.py
+++
@@ -1,6 +0,0 @@
-from tkinter import *
-root = Tk()
-root.title("Mellody")
-root.iconbitmap('D:/icon/favicon.ico')
-
-root.mainloop()
